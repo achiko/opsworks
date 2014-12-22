adc5f8c7fd5cf3bd8780319d8bb7cde66e9dd5c7
Archil <achiko@gmail.com>
2014-12-22 22:59:16 +0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
   
-}).listen(5000);
+}).listen(80);
 
 
-console.log('Server running at http://127.0.0.1:5000/');
+console.log('Server running at http://127.0.0.1:80/');
